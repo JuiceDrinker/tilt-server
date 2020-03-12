e478efdea65c95b26bf291d022e4fbfaae468922
Aditya Subramanian <subramanianaditya42@gmail.com>
2020-03-12 13:46:41 +0100

    Prod deploy v8

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN, "https://tilt-pod.herokuapp.com"]
+    origin: [process.env.PUBLIC_DOMAIN, "http://tilt-pod.herokuapp.com"]
   })
 );
 // app.use((req, res, next) => {
