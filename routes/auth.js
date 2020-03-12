d13a44e14641fef0d391ea94dbaac69c2bffb848
Aditya Subramanian <subramanianaditya42@gmail.com>
2020-03-12 13:28:47 +0100

    Prod deploy v5

--- routes/auth.js
+++ routes/auth.js
@@ -13,80 +13,87 @@ const {
 } = require("../helpers/middlewares");
 
 // POST '/auth/signup'
-router.post('/signup', isNotLoggedIn, validationLogin, async (req, res, next) => {
-  const { username, password } = req.body;
+router.post(
+  "/signup",
+  isNotLoggedIn,
+  validationLogin,
+  async (req, res, next) => {
+    const { username, password } = req.body;
 
-  try {																									 // projection
-    const usernameExists = await User.findOne({ username }, 'username');
-    
-    if (usernameExists) return next(createError(400));
-    else {
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hashPass = bcrypt.hashSync(password, salt);
-      const newUser = await User.create({ username, password: hashPass });
+    try {
+      // projection
+      const usernameExists = await User.findOne({ username }, "username");
 
-      newUser.password = "*";
-      req.session.currentUser = newUser;
-      res
-        .status(201)  //  Created
-        .json(newUser);
+      if (usernameExists) return next(createError(400));
+      else {
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashPass = bcrypt.hashSync(password, salt);
+        const newUser = await User.create({ username, password: hashPass });
+
+        newUser.password = "*";
+        req.session.currentUser = newUser;
+        res
+          .status(201) //  Created
+          .json(newUser);
+      }
+    } catch (error) {
+      next(createError(error));
     }
-  } 
-  catch (error) {
-    next(createError(error));
   }
-},
 );
 
 // POST '/auth/login'
-router.post('/login', isNotLoggedIn, validationLogin, async (req, res, next) => {
-  const { username, password } = req.body;
-  try {
-    const user = await User.findOne({ username }) ;
-    if (!user) {
-      next(createError(404));
-    } 
-    else if (bcrypt.compareSync(password, user.password)) {
-      
-      user.password = '*';
-      req.session.currentUser = user;
-      res
-        .status(200)
-        .json(user);
-    //return;	 			TODO - remove from the notes
-    } 
-    else {
-      next(createError(401));	// Unauthorized
+router.post(
+  "/login",
+  isNotLoggedIn,
+  validationLogin,
+  async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        next(createError(404));
+      } else if (bcrypt.compareSync(password, user.password)) {
+        user.password = "*";
+        req.session.currentUser = user;
+        res.status(200).json(user);
+        //return;	 			TODO - remove from the notes
+      } else {
+        next(createError(401)); // Unauthorized
+      }
+    } catch (error) {
+      next(createError(error));
     }
-  } 
-  catch (error) {
-    next(createError(error));
   }
-},
 );
 
 // POST '/auth/logout'
-router.post('/logout', isLoggedIn, (req, res, next) => {
-  req.session.destroy();
-  res
-    .status(204)  //  No Content
-    .send();
+router.post("/logout", isLoggedIn, (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res
+        .status(204) //  No Content
+        .send();
+    }
+  });
 });
 
 // GET '/auth/me'
-router.get('/me', isLoggedIn, (req, res, next) => {
+router.get("/me", isLoggedIn, (req, res, next) => {
   // const currentUserSessionData = req.session.currentUser;
   // currentUserSessionData.password = '*';
-  
+
   // res.status(200).json(currentUserSessionData);
-  const {_id} = req.session.currentUser
-  User.findById(_id).populate('listenedEpisodes')
-    .then(currentUser=>{
+  const { _id } = req.session.currentUser;
+  User.findById(_id)
+    .populate("listenedEpisodes")
+    .then(currentUser => {
       // currentUser.password = '*'
-      res.status(200).json(currentUser)
+      res.status(200).json(currentUser);
     })
-    .catch(err=> console.log(err))
-    
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
